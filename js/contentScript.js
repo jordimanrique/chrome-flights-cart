97cb7d19e3a8528dc7e6c04b3ccc2330fec862f2
Jordi Manrique <jordi.manrique@atrapalo.com>
2018-01-20 13:42:17 +0100

    all domains checked and informed

--- js/contentScript.js
+++ js/contentScript.js
@@ -2,6 +2,10 @@ const storage = chrome.storage.local;
 
 console.log('carrito chrome extension');
 
+const CP = '08021';
+const ADDRESS = 'Calle aribau 185, 1a';
+const PHONE = '+34 646 64 64 64';
+const CITY = 'Barcelona';
 const SURNAME = 'Marpinez';
 const NAMES = [
     { gender : 'mrs', name : 'Sara' },
@@ -57,27 +61,41 @@ function fillForm() {
       $('#mp2_nombre_reg').val(userDefault.name);
       $('#mp2_email_reg').val(userDefault.email);
       $('#mp2_email_reg2').val(userDefault.email);
-      $('#mp2_cp_reg').val('08001');
-      $('#mp2_direccion_reg').val('Carrer de les proves de ticketing');
-      $('#mp2_poblacion_reg').val('Barcelona');
+      $('#mp2_cp_reg').val(CP);
+      $('#mp2_direccion_reg').val(ADDRESS);
+      $('#mp2_poblacion_reg').val(CITY);
       $('#mp2_regione_reg').val('1').trigger('change');
-      $('#mp2_movil_reg').val('+34 646 64 64 64');
+      $('#mp2_movil_reg').val(PHONE);
       $('#mp2_apellidos_reg').val(SURNAME);
 
       $('#check_addons_rechaza').prop("checked", true);
       $('#check_seguro_cancelacion_rechaza').prop("checked", true);
 
-      $('#sync-payer-with-asistente-0').click();
-
       $('#mp4_num_tarjeta').val('4548812049400004');
       $('#mp4_Month').val('04').trigger('change');
       $('#mp4_Year').val(((new Date).getFullYear() + 1).toString().substr(-2)).trigger('change');
       $('#mp4_cvv').val('123');
-      $('#mp4_tipo_tarjeta').val('VID').trigger('change');
+
+
+      if($("#mp4_tipo_tarjeta option[value='VID']").length > 0) {
+        $('#mp4_tipo_tarjeta').val('VID').trigger('change');
+      } else if ($("#mp4_tipo_tarjeta option[value='VI']").length > 0) {
+        $('#mp4_tipo_tarjeta').val('VI').trigger('change');
+      }
+
+      $('#mp312_situacion_fiscal').val('5').trigger('change');
+      $('#mp312_direccion').val(ADDRESS);
+      $('#mp312_telefono').val(PHONE);
+      $('#mp312_ciudad').val(CITY);
+      $('#mp312_provincia').val(CITY);
+      $('#mp312_cp').val(CP);
+
       // $(window).scrollTop($('#fsCarritoBottom').offset().top);
 
       setTimeout(function () {
         $('#btn_finalizar_continuar').click();
+        $('#sync-payer-with-asistente-0').click();
+        $('#mp312_use_first_asistant_data').click();
       }, 1000);
     }
 }
@@ -95,6 +113,9 @@ function fillUserInfo(index, userInfo) {
     $('#mp3_'+ index +'_caducidad_documento_dia').val($('#mp3_'+ index +'_caducidad_documento_dia option:eq(1)').val());
     $('#mp3_'+ index +'_caducidad_documento_mes').val($('#mp3_'+ index +'_caducidad_documento_mes option:eq(1)').val());
     $('#mp3_'+ index +'_caducidad_documento_anyo').val($('#mp3_'+ index +'_caducidad_documento_anyo option:eq(3)').val());
+
+    $('#mp3_tipo_documento_' + index).val(2).trigger('change');
+    $('#mp3_num_documento_' + index).val('1234567');
 
     setTimeout(() => {
         $('#mp3_apellidos_' + index).val(SURNAME).trigger('click');
